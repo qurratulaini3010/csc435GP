88575bd47c5552f42ebd32b0e64bee9a2d345238
jihaao <nazihahnajah@gmail.com>
2025-06-23 14:53:15 +0800

    Delete Lorry.java

--- Lorry.java
+++
@@ -1,5 +0,0 @@
-public class Lorry extends Vehicle {
-    public Lorry(String model, String plateNumber, double pricePerDay, boolean available) {
-        super(model, plateNumber, pricePerDay, available);
-    }
-}
