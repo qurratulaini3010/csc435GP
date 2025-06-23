7b1aac7b32701d36a639f8b5eb24a442aed1b7a5
jihaao <nazihahnajah@gmail.com>
2025-06-23 14:50:56 +0800

    Add files via upload

--- Lorry.java
+++ Lorry.java
@@ -1,20 +1,5 @@
 public class Lorry extends Vehicle {
-    private double maxLoad;
-    private boolean hasTailLift;
-
-    public Lorry(String model, String plateNumber, double pricePerDay,  double maxLoad, boolean hasTailLift) {
-        super(model, plateNumber, pricePerDay);
-        this.vehicleType = "Lorry";
-        this.maxLoad = maxLoad;
-        this.hasTailLift = hasTailLift;
-    }
-
-    public double getMaxLoad() { return maxLoad; }
-    public boolean hasTailLift() { return hasTailLift; }
-    
-    @Override
-    public String toString() {
-        return super.toString() + String.format(" - Max Load: %.1f tons - Tail Lift: %s",
-            maxLoad, hasTailLift ? "Yes" : "No");
+    public Lorry(String model, String plateNumber, double pricePerDay, boolean available) {
+        super(model, plateNumber, pricePerDay, available);
     }
 }
